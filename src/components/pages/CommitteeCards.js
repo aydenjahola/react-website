2f5b7b71f6e16197f2075bb1465a97e63a67a7e8
Jed Hazaymeh <jed.hazaymeh@gmail.com>
2022-11-15 16:57:55 +0000

    Update committee photos

--- src/components/pages/CommitteeCards.js
+++ src/components/pages/CommitteeCards.js
@@ -8,19 +8,19 @@ function CommitteeCards() {
             <div className='committee__cards__wrapper'>
                 <ul className='committee__cards__items'>
                     <CommitteeCardItem
-                        src='images/default.png'
+                        src='images/committee/amy.jpg'
                         text='Chairperson'
                         label='Amy Collins'
                         path='/contact'
                     />
                     <CommitteeCardItem
-                        src='images/default.png'
+                        src='images/committee/andreea.png'
                         text='Vice-Chairperson'
                         label='Andreea Rete'
                         path='/contact'
                     />
                     <CommitteeCardItem
-                        src='images/default.png'
+                        src='images/committee/om.jpg'
                         text='Secretary'
                         label='Om Dighe'
                         path='/contact'
@@ -28,19 +28,19 @@ function CommitteeCards() {
                 </ul>
                 <ul className='cards__items'>
                     <CommitteeCardItem
-                        src='images/default.png'
+                        src='images/committee/malachy.jpg'
                         text='SysAdmin'
                         label='Malachy Byrne'
                         path='/contact'
                     />
                     <CommitteeCardItem
-                        src='images/default.png'
+                        src='images/committee/james.jpg'
                         text='SysAdmin'
                         label='James Hackett'
                         path='/contact'
                     />
                     <CommitteeCardItem
-                        src='images/default.png'
+                        src='images/committee/conor.jpg'
                         text='SysAdmin'
                         label='Conor Joyce'
                         path='/contact'
@@ -48,19 +48,19 @@ function CommitteeCards() {
                 </ul>
                 <ul className='cards__items'>
                     <CommitteeCardItem
-                        src='images/default.png'
+                        src='images/committee/gordon.jpg'
                         text='Treasurer'
                         label='Gordon Chi'
                         path='/contact'
                     />
                     <CommitteeCardItem
-                        src='images/default.png'
+                        src='images/committee/jed.jpg'
                         text='Webmaster'
                         label='Jed Hazaymeh'
                         path='/contact'
                     />
                     <CommitteeCardItem
-                        src='images/default.png'
+                        src='images/committee/anushree.jpg'
                         text='GDO'
                         label='Anushree Umak'
                         path='/contact'
@@ -68,19 +68,19 @@ function CommitteeCards() {
                 </ul>
                 <ul className='committee__cards__items'>
                     <CommitteeCardItem
-                        src='images/default.png'
+                        src='images/committee/abhijit.jpg'
                         text='Events Officer'
                         label='Abhijit Mahal'
                         path='/contact'
                     />
                     <CommitteeCardItem
-                        src='images/default.png'
+                        src='images/committee/cheehin.jpg'
                         text='Events Officer'
                         label='Chee Hin Choa'
                         path='/contact'
                     />
                     <CommitteeCardItem
-                        src='images/default.png'
+                        src='images/committee/dominic.jpg'
                         text='First Year Rep.'
                         label='Dominic Michael Connor'
                         path='/contact'
@@ -88,25 +88,25 @@ function CommitteeCards() {
                 </ul>
                 <ul className='committee__cards__items'>
                     <CommitteeCardItem
-                        src='images/default.png'
+                        src='images/committee/aidan.png'
                         text='Helpdesk'
                         label='Aidan Smith'
                         path='/contact'
                     />
                     <CommitteeCardItem
-                        src='images/default.png'
+                        src='images/committee/malavika.jpeg'
                         text='Helpdesk'
                         label='Malavika Shanker'
                         path='/contact'
                     />
               	    <CommitteeCardItem
-                        src='images/default.png'
+                        src='images/committee/leo.jpg'
                         text='Ordinary Member'
                         label='Leo Collins'
                         path='/contact'
                     />
                     <CommitteeCardItem
-                        src='images/default.png'
+                        src='images/committee/alex.jpg'
                         text='Ordinary Member'
                         label='Alexandru Dorofte'
                         path='/contact'
