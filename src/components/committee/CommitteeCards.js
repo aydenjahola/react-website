af0481750afe7227ebfeb7ffb99cc706a6d71cae
Jed Hazaymeh <jed.hazaymeh2@mail.dcu.ie>
2023-11-21 13:59:16 +0000

    Update committee photos

--- src/components/committee/CommitteeCards.js
+++ src/components/committee/CommitteeCards.js
@@ -1,126 +1,132 @@
-import React from 'react';
-import './CommitteeCards.css';
-import CommitteeCardItem from './CommitteeCardItem.js';
+import React from 'react'
+import './CommitteeCards.css'
+import CommitteeCardItem from './CommitteeCardItem.js'
 
 function CommitteeCards() {
-    return (
-        <div className='committee__cards__container'>
-            <div className='committee__cards__wrapper'>
-                <ul className='committee__cards__items'>
-                    <CommitteeCardItem
-                        src='images/committee/amy.jpg'
-                        text='Chairperson'
-                        label='Amy Collins'
-                        path='/contact'
-                    />
-                    <CommitteeCardItem
-                        src='images/committee/andreea.png'
-                        text='Vice-Chairperson'
-                        label='Andreea Rete'
-                        path='/contact'
-                    />
-                    <CommitteeCardItem
-                        src='images/committee/om.jpg'
-                        text='Secretary'
-                        label='Om Dighe'
-                        path='/contact'
-                    />
-                </ul>
-                <ul className='cards__items'>
-                    <CommitteeCardItem
-                        src='images/committee/malachy.jpg'
-                        text='SysAdmin'
-                        label='Malachy Byrne'
-                        path='/contact'
-                    />
-                    <CommitteeCardItem
-                        src='images/committee/james.jpg'
-                        text='SysAdmin'
-                        label='James Hackett'
-                        path='/contact'
-                    />
-                    <CommitteeCardItem
-                        src='images/committee/conor.jpg'
-                        text='SysAdmin'
-                        label='Conor Joyce'
-                        path='/contact'
-                    />
-                </ul>
-                <ul className='cards__items'>
-                    <CommitteeCardItem
-                        src='images/committee/gordon.jpg'
-                        text='Treasurer'
-                        label='Gordon Chi'
-                        path='/contact'
-                    />
-                    <CommitteeCardItem
-                        src='images/committee/jed.jpg'
-                        text='Webmaster'
-                        label='Jed Hazaymeh'
-                        path='/contact'
-                    />
-                    <CommitteeCardItem
-                        src='images/committee/anushree.jpg'
-                        text='GDO'
-                        label='Anushree Umak'
-                        path='/contact'
-                    />
-                </ul>
-                <ul className='committee__cards__items'>
-                    <CommitteeCardItem
-                        src='images/committee/ishita.jpg'
-                        text='PRO'
-                        label='Ishita Gupta'
-                        path='/contact'
-                    />
-                    <CommitteeCardItem
-                        src='images/committee/abhijit.jpg'
-                        text='Events Officer'
-                        label='Abhijit Mahal'
-                        path='/contact'
-                    />
-                    <CommitteeCardItem
-                        src='images/committee/cheehin.jpg'
-                        text='Events Officer'
-                        label='Chee Hin Choa'
-                        path='/contact'
-                    />
-                    <CommitteeCardItem
-                        src='images/committee/dominic.jpg'
-                        text='First Year Rep.'
-                        label='Dominic Michael Connor'
-                        path='/contact'
-                    />
-                </ul>
-                <ul className='committee__cards__items'>
-                    <CommitteeCardItem
-                        src='images/committee/aidan.png'
-                        text='Helpdesk'
-                        label='Aidan Smith'
-                        path='/contact'
-                    />
-                    <CommitteeCardItem
-                        src='images/committee/malavika.jpeg'
-                        text='Helpdesk'
-                        label='Malavika Shanker'
-                        path='/contact'
-                    />
-                    <CommitteeCardItem
-                        src='images/committee/leo.jpg'
-                        text='Ordinary Member'
-                        label='Leo Collins'
-                        path='/contact'
-                    />
-                    <CommitteeCardItem
-                        src='images/committee/alex.jpg'
-                        text='Ordinary Member'
-                        label='Alexandru Dorofte'
-                        path='/contact'
-                    />
-                </ul>
-            </div>
+  return (
+    <>
+      <p class='content-title'>COMMITTEE</p>
+      <hr class='full'></hr>
+      <br />
+      <br />
+      <div className='committee__cards__container'>
+        <div className='committee__cards__wrapper'>
+          <ul className='committee__cards__items'>
+            <CommitteeCardItem
+              src='images/committee/james.jpg'
+              text='Chairperson'
+              label='James Hackett'
+              path='/contact'
+            />
+            <CommitteeCardItem
+              src='images/committee/shane.jpg'
+              text='Vice-Chairperson'
+              label='Shane Whelan'
+              path='/contact'
+            />
+            <CommitteeCardItem
+              src='images/committee/jake.jpg'
+              text='Secretary'
+              label='Jake Farrell'
+              path='/contact'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CommitteeCardItem
+              src='images/committee/dominic.png'
+              text='SysAdmin'
+              label='Dominic Michael Connor'
+              path='/contact'
+            />
+            <CommitteeCardItem
+              src='images/committee/cathal.jpg'
+              text='SysAdmin'
+              label="Cathal O'Grady"
+              path='/contact'
+            />
+            <CommitteeCardItem
+              src='images/committee/amy.jpg'
+              text='SysAdmin'
+              label='Amy Collins'
+              path='/contact'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CommitteeCardItem
+              src='images/committee/malavika.jpg'
+              text='Treasurer'
+              label='Malavika Shanker'
+              path='/contact'
+            />
+            <CommitteeCardItem
+              src='images/committee/jed.png'
+              text='Webmaster'
+              label='Jed Hazaymeh'
+              path='/contact'
+            />
+            <CommitteeCardItem
+              src='images/committee/ishita.jpg'
+              text='GDO'
+              label='Ishita Gupta'
+              path='/contact'
+            />
+          </ul>
+          <ul className='committee__cards__items'>
+            <CommitteeCardItem
+              src='images/committee/philip.jpg'
+              text='PRO'
+              label='Philip Leonard'
+              path='/contact'
+            />
+            <CommitteeCardItem
+              src='images/committee/robert.jpg'
+              text='Events Officer'
+              label='Robert Healy'
+              path='/contact'
+            />
+            <CommitteeCardItem
+              src='images/committee/stefania.jpg'
+              text='Events Officer'
+              label='Stefania'
+              path='/contact'
+            />
+            <CommitteeCardItem
+              src='images/committee/dennis.jpg'
+              text='First Year Rep.'
+              label='Dennis Custiuc'
+              path='/contact'
+            />
+          </ul>
+          <ul className='committee__cards__items'>
+            <CommitteeCardItem
+              src='images/committee/ayden.png'
+              text='Helpdesk'
+              label='Ayden Jahola'
+              path='/contact'
+            />
+            <CommitteeCardItem
+              src='images/committee/francis.jpg'
+              text='Helpdesk'
+              label='Francis Baxter'
+              path='/contact'
+            />
+            <CommitteeCardItem
+              src='images/committee/callum.jpg'
+              text='Ordinary Member'
+              label='Callum Browne'
+              path='/contact'
+            />
+            <CommitteeCardItem
+              src='images/committee/daniel.jpg'
+              text='Ordinary Member'
+              label='Daniel McEntee'
+              path='/contact'
+            />
+          </ul>
         </div>
-    );
+      </div>
+    </>
+  )
 }
 
-export default CommitteeCards;
+export default CommitteeCards
